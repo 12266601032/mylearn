584665911ced31c5820c7a7e7b414faaf57317e7
liucongcong <liucongcong@qq.com>
2018-03-21 18:27:43 +0800

    typeToken

--- learn-code/src/test/java/com/example/lcc/basic/guava/ReflectionDemo.java
+++ learn-code/src/test/java/com/example/lcc/basic/guava/ReflectionDemo.java
@@ -1,10 +1,20 @@
 package com.example.lcc.basic.guava;
 
+import com.google.common.collect.Lists;
 import com.google.common.reflect.Reflection;
+import com.google.common.reflect.TypeResolver;
+import com.google.common.reflect.TypeToken;
 import org.junit.Test;
+import org.omg.CORBA.Any;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
+import javax.lang.model.type.TypeVisitor;
+import java.io.Serializable;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by liucongcong on 2018/3/13.
@@ -37,7 +47,42 @@ public class ReflectionDemo {
         Reflection.initialize(Say.class);
         //com.google.common.reflect.Reflection = com.google.common.reflect
         System.out.println(Reflection.getPackageName(Say.class));
+
     }
+
+    @Test
+    public void typeResolver(){
+        TypeResolver typeResolver = new TypeResolver();
+        AnyType<Long> stringAnyType = new AnyType<>();
+
+
+        TypeVariable<? extends Class<? extends AnyType>>[] typeParameters = stringAnyType.getClass().getTypeParameters();
+        TypeVariable t = typeParameters[0];
+        System.out.println(t);
+        System.out.println(t.getBounds().length);
+        System.out.println(Arrays.toString(t.getBounds()));
+        System.out.println(t instanceof ParameterizedType);
+
+        TypeToken<List<String>> typeToken = new TypeToken<List<String>>() {};
+        TypeToken<?> typeToken1 = typeToken.resolveType(Long.class);
+        System.out.println(typeToken1);
+        System.out.println(typeToken.getRawType());
+        System.out.println(typeResolver.resolveType(typeToken.getType()));
+
+        TypeToken<AnyType<Long>> typeToken2 = new TypeToken<AnyType<Long>>() {};
+        System.out.println(((ParameterizedType)typeToken2.getSupertype(AnyType.class).getType()).getActualTypeArguments()[0]);
+
+    }
+
+    public static class AnyType<T extends Serializable>{
+
+    }
+
+    public static class SubType<Long> extends AnyType{
+
+    }
+
+
 
     public static interface Say {
         void hello();
