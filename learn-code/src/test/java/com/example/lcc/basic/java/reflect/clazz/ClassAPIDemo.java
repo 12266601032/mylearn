b58aa845cc790b32ef24aaf944f76cc8b3a46f28
liucongcong <liucongcong@qq.com>
2018-04-28 11:50:23 +0800

    update

--- learn-code/src/test/java/com/example/lcc/basic/java/reflect/clazz/ClassAPIDemo.java
+++ learn-code/src/test/java/com/example/lcc/basic/java/reflect/clazz/ClassAPIDemo.java
@@ -2,21 +2,6 @@ package com.example.lcc.basic.java.reflect.clazz;
 
 import org.junit.Test;
 
-<<<<<<< HEAD
-import java.math.BigDecimal;
-
-/**
- * @date 2018/4/24
- */
-public class ClassAPIDemo {
-
-
-    @Test
-    public void testAssignable(){
-        System.out.println(BigDecimal.class.isAssignableFrom(null)); //NullPointerException
-    }
-
-=======
 import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Locale;
@@ -32,5 +17,4 @@ public class ClassAPIDemo {
         System.out.println(Map.class.isAssignableFrom(HashMap.class)); //true
         System.out.println(Enum.class.isAssignableFrom(RoundingMode.class));//true
     }
->>>>>>> 0aafe3de1fd832dc873a04c701dbd5fc9807395a
 }
